953ac598e1c5940ddbe2ee17f70788a92dbb9677
Iury Lima Rosal <iuryrosal@gmail.com>
2022-09-02 22:19:12 -0300

    ✨ feat: Add init to Flask App and Database Connection

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from src.conf.config import config_variables
+from src.models.vehicle import *
+
+config = config_variables()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = config["CONN_STR"]
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.app_context().push()
+database.init_app(app)
+database.create_all()
+database.session.commit()
