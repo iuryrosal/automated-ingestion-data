f5d290b04d5019f89173f72cd6962c7e5c6b9d12
Iury Lima Rosal <iuryrosal@gmail.com>
2022-09-04 16:24:56 -0300

    ✅ test: Add test_database

---
+++ tests/test_database.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch, call
+
+from src.data.database import Database
+from src.conf.config import config_variables
+
+conn_str_expected = "postgresql+psycopg2://root:password@localhost:5432/data_vehicles"
+
+@patch.object(Database, 'create_engine_db')
+def test_conn(mock_create_engine_db):
+  expected_calls = [call(conn_str_expected),
+                    call().connect(),
+                    call(conn_str_expected),
+                    call().connect()]
+
+  Database(config_variables())
+
+  mock_create_engine_db.assert_has_calls(expected_calls)
+
+@patch.object(Database, 'connect_db')
+def test_build_conn_str(mock_connect_db):
+  database = Database(config_variables())
+  conn_str = database.build_conn_str()
+
+  assert conn_str_expected == conn_str 
