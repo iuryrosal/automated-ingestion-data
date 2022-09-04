7001b1bb42bb58a55ba9f1eda09fa4acf4e4fec6
Iury Lima Rosal <iuryrosal@gmail.com>
2022-09-04 10:24:38 -0300

    🔊 chore: Add logging in init file

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from src.conf.config import config_variables
+from src.data.ingestion_process import IngestionProcess
 from src.models.vehicle import *
+import logging
 
+logging.basicConfig(filename="api.log",
+                    level=logging.DEBUG,
+                    format="%(asctime)s :: %(levelname)s :: %(filename)s :: %(lineno)d :: %(message)s")
+
+logging.info("Loading app config...")
 config = config_variables()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = config["CONN_STR"]
@@ -11,3 +18,4 @@ database.init_app(app)
 database.create_all()
 database.session.commit()
 ingestion_process = IngestionProcess("data/raw/*.csv", config)
+logging.info("App config loaded.")
