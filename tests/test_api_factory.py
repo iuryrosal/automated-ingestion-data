88a83070b13468f5ffde2c23b1c2e19180c16bc6
Iury Lima Rosal <iuryrosal@gmail.com>
2022-09-04 16:22:26 -0300

    ✅ test: Add test_api_factory

---
+++ tests/test_api_factory.py
@@ -0,0 +1,7 @@
+from src.data.database import Database
+
+def test_api_is_created(app):
+  assert app.name == "src.utils.api_factory"
+
+def test_request_returns_404(app):
+  assert app.test_client().get("/url_not_exists").status_code == 404
