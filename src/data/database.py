4ec18bbb60001b8038cbff5545681595915a0d74
Iury Lima Rosal <iuryrosal@gmail.com>
2022-09-04 16:26:50 -0300

    ♻️ refactor: Create functions of create app and database

--- src/data/database.py
+++ src/data/database.py
@@ -4,16 +4,22 @@ import pandas as pd
 class Database:
   def __init__(self, config_dict: dict) -> None:
     self.conf = config_dict
-    self.connection_attr = self.__connect_db()
+    self.connection_attr = self.connect_db()
+  
+  def build_conn_str(self):
+    return (f"postgresql+psycopg2://{self.conf['POSTGRES_USER']}:" + 
+            f"{self.conf['POSTGRES_PASSWORD']}@" +
+            f"{self.conf['POSTGRES_HOST']}:" +
+            f"{self.conf['POSTGRES_PORT']}/" +
+            f"{self.conf['POSTGRES_DB']}")
 
-  def __connect_db(self):
-    conn_str = (f"postgresql+psycopg2://{self.conf['POSTGRES_USER']}:" + 
-               f"{self.conf['POSTGRES_PASSWORD']}@" +
-               f"{self.conf['POSTGRES_HOST']}:" +
-               f"{self.conf['POSTGRES_PORT']}/" +
-               f"{self.conf['POSTGRES_DB']}")
-
+  def create_engine_db(self, conn_str):
     engine = create_engine(conn_str)
+    return engine
+
+  def connect_db(self):
+    conn_str = self.build_conn_str()
+    engine = self.create_engine_db(conn_str)
     connection = engine.connect()
     return {"CONN_STR": conn_str,
             "ENGINE": engine,
---
+++ src/utils/api_factory.py
@@ -0,0 +1,16 @@
+from src.models.vehicle import *
+from flask import Flask
+
+
+def create_app(config: dict) -> Flask:
+  app = Flask(__name__)
+  app.config["SQLALCHEMY_DATABASE_URI"] = config["CONN_STR"]
+  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+  app.app_context().push()
+  return app
+
+def init_database(app: Flask) -> SQLAlchemy:
+  database.init_app(app)
+  database.create_all()
+  database.session.commit()
+  return database
