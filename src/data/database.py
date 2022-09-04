d0e8ab17f33ff9b1bcd4d2f21283ee65f29825aa
Iury Lima Rosal <iuryrosal@gmail.com>
2022-09-04 20:08:44 -0300

    ♻️ refactor: Add database context based on env variable

--- src/data/database.py
+++ src/data/database.py
@@ -7,11 +7,14 @@ class Database:
     self.connection_attr = self.connect_db()
   
   def build_conn_str(self):
+        if self.conf["ENV"] != "local":
     return (f"postgresql+psycopg2://{self.conf['POSTGRES_USER']}:" + 
             f"{self.conf['POSTGRES_PASSWORD']}@" +
             f"{self.conf['POSTGRES_HOST']}:" +
             f"{self.conf['POSTGRES_PORT']}/" +
-            f"{self.conf['POSTGRES_DB']}")
+                    f"{self.conf['POSTGRES_DB']}")
+        else:
+            return "sqlite://"
 
   def create_engine_db(self, conn_str):
     engine = create_engine(conn_str)
