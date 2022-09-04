1f7e3334ee2e13e9fb67eb4e6716c565704aaeee
Iury Lima Rosal <iuryrosal@gmail.com>
2022-09-04 16:29:54 -0300

    feat: Update script os tests

--- main_test.py
+++ main_test.py
@@ -1,17 +1,21 @@
 from src.data.database import Database
 from src.conf.config import config_variables
+from src.data.ingestion_process import IngestionProcess
+
 
 config = config_variables()
 
 query_attr = {}
 query_attr["dataset"] = "teste"
 
+ing = IngestionProcess("data/raw/*.csv", config)
+#ing.start()
+print("processo iniciado")
 
 database = Database(config_dict=config)
 conn = database.get_conn_att()["CONNECTION"]
-results = conn.execute("""SELECT * FROM information_schema.tables 
-                        WHERE table_schema = 'public'""")
-
-results_2 = conn.execute("""SELECT * FROM vehicles_records""")
+results_2 = conn.execute("""
+                            SELECT * FROM vehicles_records
+                        """)
 for record in results_2:
     print("\n", record)
