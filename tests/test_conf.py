7c6b7e179a865be35564fdb591cf932b042f5938
Iury Lima Rosal <iuryrosal@gmail.com>
2022-09-04 16:24:28 -0300

    ✅ test: Add test_conf

---
+++ tests/test_conf.py
@@ -0,0 +1,28 @@
+from src.conf.config import config_variables
+from unittest.mock import patch
+
+# Local environment
+env = {
+    "POSTGRES_USER": "root",
+    "POSTGRES_PASSWORD": "password",
+    "POSTGRES_HOST": "localhost",
+    "POSTGRES_PORT": 5432,
+    "POSTGRES_DB": "data_vehicles",
+    "CONN_STR": "postgresql+psycopg2://root:password@localhost:5432/data_vehicles",
+    "CONNECTION": "conn",
+    "ENGINE": "engine"
+  }
+
+def get_conn_att():
+  return {
+    "CONN_STR": "postgresql+psycopg2://root:password@localhost:5432/data_vehicles",
+    "CONNECTION": "conn",
+    "ENGINE": "engine"
+  }
+
+@patch("src.data.database.Database.get_conn_att")
+def test_config_local_env(mock_get_conn_att):
+  mock_get_conn_att.return_value = get_conn_att()
+  config = config_variables()
+  config_dict_expected = env
+  assert config_dict_expected == config
